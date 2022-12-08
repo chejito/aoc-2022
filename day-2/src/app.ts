e80f275992334b41a5b17a32197c7dcf663caf6d
Sergio Méndez <smndz81@gmail.com>
2022-12-08 18:35:56 +0100

    Refactored part 1 day 2

--- day-2/src/app.ts
+++ day-2/src/app.ts
@@ -2,14 +2,13 @@ import { readFileSync } from 'fs';
 import * as path from 'path';
 
 const inputPath: string = path.resolve(__dirname, './input.txt');
-// const inputPath: string = path.resolve(__dirname, './test.txt');
 
 enum MoveItems {
   'A' = 1,
-  'X' = 1,
   'B' = 2,
-  'Y' = 2,
   'C' = 3,
+  'X' = 1,
+  'Y' = 2,
   'Z' = 3,
 }
 
@@ -19,26 +18,22 @@ enum Results {
   Loss = 0,
 }
 
-function readFile(filename: string): string[] {
+const readFile = (filename: string): string[] => {
   return readFileSync(filename, 'utf-8')
     .split(/\r?\n/);
 }
 
-function getItems(move: string): string[] {
-  return move.split(' '); 
-}
-
-function replaceWithNames(letters: string[]): number[] {
+const replaceWithValues = (letters: string[]): number[] => {
   return letters.map(letter => MoveItems[letter]);
 }
 
-function getMoveResult(move: number[]): number {
+const getMovePoints = (move: number[]): number => {
   if (move[0] == move[1]) return Results.Draw + move[1];
   else if (checkWin(move[0], move[1])) return Results.Win + move[1];
   return Results.Loss + move[1];
 }
 
-function checkWin(elfMove: number, myMove: number): boolean {
+const checkWin = (elfMove: number, myMove: number): boolean => {
   if ((elfMove == 1 && myMove == 2) 
   || (elfMove == 2 && myMove == 3) 
   || (elfMove == 3 && myMove == 1)){
@@ -46,22 +41,8 @@ function checkWin(elfMove: number, myMove: number): boolean {
   }
 }
 
-const content = readFile(inputPath);
-const items = content.map(i => getItems(i));
-const numbers = items.map(i => replaceWithNames(i));
-const points = numbers.map(n => getMoveResult(n));
-const totalPoints = points.reduce((sum: number, num: number) => sum + num, 0);
-
-console.log('\nMoves:');
-content.forEach(action => console.log(action));
-
-console.log('\nMoves:');
-items.forEach(i => console.log(`Elf: ${i[0]} \tMe: ${i[1]}`));
-
-console.log('\nMoves:');
-numbers.forEach(i => console.log(`Elf: ${i[0]} \tMe: ${i[1]}`));
-
-console.log('\nMoves:');
-points.forEach(p => console.log(`Move points: ${p}`));
+const totalPoints = readFile(inputPath)
+  .map(move => getMovePoints(replaceWithValues(move.split(' '))))
+  .reduce((sum: number, num: number) => sum + num, 0);
 
 console.log(`\nTotal points: $${totalPoints}`);
